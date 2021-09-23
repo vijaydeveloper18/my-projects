d4c3d57abb1b18f0f0106a27072bee15f055f1c6
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2021-09-23 21:47:55 +0530

    Add files via upload

---
+++ .py coder.py
@@ -0,0 +1,12 @@
+name=raw_input("Name:")
+age=int(input("Age:"))
+qual=raw_input("Qualifiaction:")
+clg=raw_input("College:")
+reg=raw_input("Religion:")
+percent=int(input("Percentage:"))
+print name,'\t'
+print age,'\t'
+print qual,'\t'
+print clg,'\t'
+print reg,'\t'
+print percent,'\t'
