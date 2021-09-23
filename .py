75bdda96a00ac22409632c734c5b24549237474e
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2021-09-23 21:46:52 +0530

    Add files via upload
    python

---
+++ .py
@@ -0,0 +1,14 @@
+circle=int(input("area of circle:"))
+radius=3
+cirarea=3.14*radius*radius
+print cirarea,'\t'
+tri=int(input("area of triangle:"))
+breadth=20
+height=30
+triarea=0.5*(breadth*height)
+print triarea,'\t'
+rect=int(input("area of rectangle:"))
+length=30
+breadth=10
+rectarea=length*breadth
+print rectarea,'\t'
