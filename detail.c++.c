2ae4ff7b660031053a1e1fcea7db9a168a9c956b
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2021-09-24 20:27:51 +0530

    Add files via upload

---
+++ detail.c++.c
@@ -0,0 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    char name[30],qualification[20];
+    int age,height;
+    cout<<"Enter the name:";
+    cin>>name;
+    cout<<"Enter the age:";
+    cin>>age;
+    cout<<"Qualification:";
+    cin>>qualification;
+    cout<<"Height:";
+    cin>>height;
+    cout<<name<<endl;
+    cout<<age<<endl;
+    cout<<qualification<<endl;
+    cout<<height<<endl;
+    getch();
+}
