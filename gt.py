d94c19840c338f29d7fdbda58f8d4b8f7674e1f6
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2021-10-01 21:08:14 +0530

    python

---
+++ gt.py
@@ -0,0 +1,9 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+if(a>b)and(a>c):
+    print("Greatest is A")
+    elif(b>c):
+        print("Greatest is B")
+        else:
+            print("Greatest is C")
