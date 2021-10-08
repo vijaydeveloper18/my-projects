243593eea78565b1165cf72469f1577c2c687a3b
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2021-10-08 20:37:33 +0530

    count of alphabet in python

---
+++ co.py
@@ -0,0 +1,8 @@
+def alpha(a):
+    count=0
+    for i in a(0,1):
+           count=count+1
+           if(letters==alpha):
+               letters+=1
+a=input("Type:")
+print("No.of.letters:%d",count(a))
