a511290bd600432a3e238a41c65a5339356f4e89
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2021-10-22 21:06:45 +0530

    python built in function

---
+++ built.py
@@ -0,0 +1,10 @@
+Nset={1,2,3,4,5,6,7,8,9}
+print ("\n actual set values=",Nset)
+print("\n maximum of nset={}".format(max(Nset)))
+print("\n minimum of nset={}".format(min(Nset)))
+print("\n sum of nset={}".format(sum(Nset)))
+print("\n any false value in nset={}".format(any(Nset)))
+print("\n any true value in nset={}".format(all(Nset)))
+print("\n sorted value of nset={}".format(sorted(Nset)))
+print("\n length of nset={}".format(len(Nset)))
+      
