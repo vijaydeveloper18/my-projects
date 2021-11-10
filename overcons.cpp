919459b215a0125b8e2f2afef5c1fb720045d112
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2021-11-10 20:19:15 +0530

    c++ using overloading constructor

---
+++ overcons.cpp
@@ -0,0 +1,33 @@
+#include<iostream.h>
+#include<conio.h>
+class circle
+{
+    private:
+     int radius;
+     float area;
+     int b,h;
+     public:
+     circle(){
+         cout<<"Enter the radius"<<endl;
+         cin>>radius;
+         area=3.14*radius*radius;
+         cout<<"area of the circle"<<area;
+     }
+}
+circle(int r){
+    radius=r;
+    area=3.14*radius*radius;
+    cout<<"\n area of circle is"<<area;}
+    circle(int x,int y){
+        b=x;
+        h=y;
+        area=0.5*b*h;
+        cout<<"\n area of triangle is"<<area;
+    }
+void main()
+{
+    circle c;
+    circle d1(6);
+    circle c1(3,4);
+    getch();
+}
