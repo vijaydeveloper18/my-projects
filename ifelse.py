55474345357665a0182b50b1ef5e33338786b5a9
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2021-11-11 20:20:41 +0530

    python

---
+++ ifelse.py
@@ -0,0 +1,14 @@
+n=int(input("Enter the number:"))
+if(n%2==1):
+    print("weird")
+    if(n%2==0):
+        print("Not weird")
+    for i in n(2,20):
+        if i(2<=5):
+            print("Not weird")
+            elif i(6<=20):
+              print("weird")
+             else:
+               print("Not weird")
+         else:
+             print("Not weird")
