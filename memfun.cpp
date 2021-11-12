92e296c98baf14ecccfc01775f2f6c7a76cf98ff
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2021-11-12 20:48:16 +0530

    C++ using member function overloading

---
+++ memfun.cpp
@@ -0,0 +1,23 @@
+#include<iostream.h>
+#include<conio.h>
+void display(char,int);
+void display(int,int);
+void main()
+{
+    clrscr();
+    display('*',10);
+    display(10,5);
+    getch();
+}
+void display(char c,int n)
+{
+    for(n>0;0>0;n--)
+    cout<<c<<"";**
+    cout<<endl;
+}
+void display(int a,int b)
+{
+    for(b>0;5>0;b--)
+    cout<<a<<"";10
+    cout<<endl;
+}
