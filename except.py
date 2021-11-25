f87285d1458fb670459ccd5b569a9b585b12ebcc
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2021-11-25 21:14:54 +0530

    Python exception

--- except.py
+++ except.py
@@ -1,18 +1,18 @@
 class myerror(exception):
     pass
-class ZeroValueError(exception):
+class zerovalueerror(exception):
     pass
 while true:
     try:
-        i_num=int(input("enter a number:"))
-        if i_num==0:
-            raise ZeroValueError
-        if i_num<0:
-            raise MyError
+        i_num=int(input("enter the number:"))
+        if(i_num==0):
+            raise zerovalueerror
+        if(i_num<0):
+            raise myerror
         else:
             print("valid input")
             break
-    except Myerror:
-        print("value should be positive number...try again!")
-    except ZeroValueError:
-        print("value should not be zero...,try again!")
+    except myerror:
+         print("value should be positive number...,try again!")
+    except zerovalueerror:
+         print("value should not be zero..,try again!")
