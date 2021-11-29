9a992ad4a2c36c43bb072785758364f7832bacfa
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2021-11-29 20:30:17 +0530

    Python using Bank details

---
+++ Bank.py
@@ -0,0 +1,22 @@
+class bank():
+    def __init__(self,accno,name,age,ifsc,state,pincode):
+        self.accno=a
+        self.name=b
+        self.age=c
+        self.ifsc=d
+        self.state=e
+        self.pin=f
+        def callme(self):
+            print("Account no:",self.accno)
+            print("Name:",self.name)
+            print("Age:",self.age)
+            print("IFSC code:",self.ifsc)
+            print("State:",self.state)
+            print("Pincode:",self.pin)
+a1=accno('289743')
+b1=name('ram')
+c1=age('22')
+d1=ifsc('10893')
+e1=state('tamilnadu')
+f1=pincode('627008')
+bank.callme()
