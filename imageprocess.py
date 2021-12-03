477d7749daaa499c5a9890f3ea6548ca4fb95b1b
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2021-12-03 20:52:27 +0530

    Image processing using python

---
+++ imageprocess.py
@@ -0,0 +1,9 @@
+im1.thumbnail((150,100))
+im1.rotate(45).show()
+from PIL import image
+img=Image.new("RGB",(100,150),"red")
+img1=dog.jpg
+img2=cat.jpg
+img_blend=Image.blend(img1,img2,0.0)
+img1.convert("L").show()
+cimg1=img1.crop(100,100,150,150)=>left,upper,right,lower
