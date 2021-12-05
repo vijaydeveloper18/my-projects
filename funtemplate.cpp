257ebf80afe065e853129fa64c1927cb807c3eb3
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2021-12-05 20:46:51 +0530

    Template function using C++

---
+++ funtemplate.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+template<class T1,class T2>
+void good(T1 x,T2 y)
+{
+    clrscr();
+    cout<<x<<""<<y<<endl;
+}
+void main(){
+    good(456,"hello");
+    getch();
+}
