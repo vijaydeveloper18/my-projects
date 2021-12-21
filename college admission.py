e95489b41e3bdb9af6a251d15e7bede252b133bf
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2021-12-21 21:19:04 +0530

    College admission form using python

---
+++ college admission.py
@@ -0,0 +1,14 @@
+class admission():
+    def __init__(self,name,age,dob,course):
+        self.name=x
+        self.age=y
+        self.dob=r
+        self.course=z
+    def callme(self):
+        print("Name:",self.name)
+        print("Age:",self.age)
+        print("Date of birth:",self.dob)
+        print("Course:",self.course)
+n=admission('rajesh','20','4.6.1999','computer.sci')
+n.callme()
+    
