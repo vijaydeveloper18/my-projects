01673c017fe0f3184621dbcfbc32246189c86885
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2021-12-22 20:57:59 +0530

    Data hiding using python

---
+++ Data hiding.py
@@ -0,0 +1,9 @@
+class myclass:
+    __Hidvar=0
+    def add(self,N):
+        self. __Hidvar=self. __Hidvar+N
+        print(self. __Hidvar)
+myobject=myclass()
+myobject.add(2)
+myobject.add(5)
+print(myobject. __Hidvar)
