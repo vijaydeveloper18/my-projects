359dc027db723ecfbe83347e9c6f1af6694ca027
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-01-05 14:50:41 +0530

    Rollar coaster allowing calculate using Javascript

---
+++ rollaride.js
@@ -0,0 +1,7 @@
+var age=16;
+var parentsPermission=true;
+if (age>=18||parentspermission==true){
+    console.log('You are allowed to rollar coaster ride');
+} else{
+    console.log('You are not allowed to rollar coaster ride');
+}
