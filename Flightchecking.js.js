812d92e137c4e17fb1aea2f56cd77db10d2c6c2a
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-01-06 20:59:43 +0530

    To check the ticket at airport using Javascript

---
+++ Flightchecking.js.js
@@ -0,0 +1,7 @@
+var checking = finish;
+var boardingpass= true;
+if(checking == finish||boardingpass== true){
+    console.log('You are allowed to enter the flight');
+}else{
+    console.log('You are not allowed to enter the flight');
+}
