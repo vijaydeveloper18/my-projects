ae4a9a4a62ea2be0791fbe476c16848e8ae258f4
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-01-10 20:50:58 +0530

    Constructor using Javascript

---
+++ cons.js
@@ -0,0 +1,9 @@
+function Employee(name,idno,salary){
+    this.name=name;
+    this.idno=idno;
+    this.salary=salary;
+}
+var employee1=new Employee('suresh',10000);
+console.log(employee1);
+var employee2=new Employee('ram',30000);
+console.log(employee2);
