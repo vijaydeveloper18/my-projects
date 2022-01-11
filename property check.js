e768631bc1e7ed258e92becf5110830e36d561e3
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-01-11 20:05:52 +0530

    Methods to check property using javascript

---
+++ property check.js
@@ -0,0 +1,9 @@
+user.name='Javascript learner';
+user.sayhi=function(){
+    console.log('Hi');
+}
+console.log(user.hasownProperty('name'));
+console.log(user.hasownProperty('sayhi'));
+console.log(user.hasownProperty('profession'));
+console.log(user.tostring());
+console.log(user.valueof());
