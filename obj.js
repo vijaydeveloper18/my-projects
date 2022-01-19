dca1b20f7b5793096647650e36a94bccc084b36a
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-01-19 21:12:12 +0530

    Object value using javascript

---
+++ obj.js
@@ -0,0 +1,15 @@
+var obj2={};
+var user1=newObject();
+user1.name='Vijay';
+user1.profession='Frontend developer';
+user1.sayhi=function(){
+    console.log('Hello' +user1.name);
+}
+user1.sayhi();
+var user2={
+    name:'Ram',
+    profession:'Entrepreneur',
+    sayhi:function(){
+        console.log('Hello' +user2.name);
+    }
+}
