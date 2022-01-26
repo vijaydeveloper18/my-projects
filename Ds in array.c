361b2edd7988573f0e078739b1deaa725f7e02b7
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-01-26 21:24:39 +0530

    Data structure using Array (removed process)

---
+++ Ds in array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int arr[N],key=40;
+    arr[N]={10,20,40,50}
+    scanf("%d",&arr[N]);
+    while(arr[N]!=key){
+      arr[n]=next;
+      next=arr[n].next;  // while searching that position
+    }
+    if(arr[N]==key){
+        arr[2]=null;    //Second position of '40' will be removed
+    }
+    return 0;
+}
