d5b15f1bde40bf3e0eb25b3f566dfd0c20f30823
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-01-28 20:40:14 +0530

    Accessing loop using javascript

---
+++ Accessloop.js
@@ -0,0 +1,10 @@
+var frontend=[5];
+frontend[0]='HTML';
+frontend[1]='CSS';
+frontend[2]='Bootstrap';
+frontend[3]='Javascript';
+frontend[4]='React js';
+var j;
+for(j of frontend){
+    alert(j);
+}
