949914f00e9d5a4da31edb4a27cf79fa5e7465e6
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-02-16 21:21:42 +0530

    Constructor using python

---
+++ cons.py
@@ -0,0 +1,9 @@
+class mul:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+        c=self.a*self.b
+def callme(self):
+    print("Multiply:",(self.a,self.b))
+m1=(2,3)
+m1.callme()    
