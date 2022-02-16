c6fc804d59c7821843f9258095e68e2d7a882115
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-02-16 21:17:57 +0530

    Typescript using Mixed destructing

---
+++ Mixed.ts
@@ -0,0 +1,17 @@
+var db={
+    name:"Harish",
+    age:18,
+    favourite:{
+        first:{
+            name:"sachin",
+            range:"best batsman"
+        },
+        second:{
+            name:"Dhoni",
+            range:"wicket keeper"
+        }
+    },    
+};
+let {favourite:{first:fav1,second:fav2};
+alert(fav1.name);
+alert(fav2.name);
