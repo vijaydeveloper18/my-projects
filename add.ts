12be934cd5b2a62e741edbc8a064e96f78f924f2
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-02-23 21:20:21 +0530

    Math concept using Typescript

---
+++ add.ts
@@ -0,0 +1,4 @@
+function add(x:number,y:number){
+    return x+y;
+}
+alert(add(25,25));
---
+++ sub.ts
@@ -0,0 +1,5 @@
+let sub:(x:number,y:number)=>number;
+sub=function(a:number,b:number):number{
+    return a-b;
+}
+alert(sub(20,80));
