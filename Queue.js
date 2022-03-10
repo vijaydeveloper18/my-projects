17f5004fc07c5435d10a0e4a5ea294cedea96f7d
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-03-10 21:13:46 +0530

    Queue data structure in array using Javascript

---
+++ Queue.js
@@ -0,0 +1,18 @@
+var arr=[3];                 //To create the array
+var front=0;rear=arr[0];
+for(var i=0;i<=3;i++){      //To check that condition
+    arr[i]=val;            //To insert data into array
+    rear++;
+}
+if(arr[lastindexof(3)]!==null && rear==arr[lastindexof(3)]){  //Data fully stored
+    console.log("The queue is full");
+}
+if(rear!=front){
+    front++;
+   for(arr[i]=3;arr[i]>=0;arr[i]--){
+    arr[i]=null;                      //Delete the datas in array
+}
+}else{
+    console.log("The queue is empty");
+}
+
