200bccbf2e3b73d9b43f261b3affb4b263bdaa97
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-03-21 12:49:18 +0530

    Date using Javascript

---
+++ date.js
@@ -0,0 +1,6 @@
+var ds=new Date();
+ds.setDate(20);
+ds.setFullYear(2022);
+document.write("After seeing date: ",ds);
+document.write("Time now in milliseconds: ",date.now());
+document.write("To display milliseconds: ",date.parse("Aug 02,1999"));
