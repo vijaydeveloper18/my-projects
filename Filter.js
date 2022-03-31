a45a8ee377cfc944ed42c3dd8c28df859d3a2f76
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-03-31 21:23:06 +0530

    Filter methods using javascript

---
+++ Filter.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        $('p[class]').css({"background-color":"pink"});
+        $('p[id]').css({"background-color":"pink"});
+    });
+});
