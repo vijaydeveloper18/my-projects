7c5929da5711511b1d7d7637deceaa2fb6914d0d
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-04-27 20:36:24 +0530

    Delete multiple records using javascript

---
+++ recorddel.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("#del").click(function(){
+        if(confirm("Are you sure to delete")){
+            var id=[];
+            $(':checkbox:checked').each(function(){
+                id[i]=$(this).val();
+            });
+            if(id.length==0){
+                alert("Select the record to delete");
+            }
+            else{
+                $.ajax({
+                    URL:"del.php",
+                    type:"post",
+                    data:{id:id},
+                    success:function(data){
+                        for(var i=0;i<id.length;i++){
+                            $('#tr'+id[i]+' ').remove();
+                        }
+                    }
+                });
+            }
+        }else{
+            return false;
+        }
+    });
+});
