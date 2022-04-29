66393ff99914ef04bee84465c2716063dd27402f
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-04-29 21:26:06 +0530

    Get values radio button using Javascript

---
+++ Getradio.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#btn").click(function(){
+        var value=$('input[name=sub]:checked').val();
+        $('p').html("<br/>Selected radio button value is:<b>"+value+"</b>");
+    })
+})
