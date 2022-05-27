d1051ff896d285248aedfffadbd6f7d2dad7a1f2
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-05-27 21:09:54 +0530

    Example code for toggle string in javascript

---
+++ exstring.js
@@ -0,0 +1,10 @@
+var str="";
+var len=str.length;
+for(i=0;i<len;i++){
+    if(len[i]==Uppercase){
+        str.toLowerCase();
+    }
+    if(len[i]==Lowercase){
+        str.toUpperCase();
+    }
+}
