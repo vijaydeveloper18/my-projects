b3ee2eb6c50f1aa2cb44c4204a84811030c56e96
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-05-27 21:10:42 +0530

    Another example code in javascript

---
+++ aegstr.js
@@ -0,0 +1,11 @@
+function change(str){
+    var len=str.length;
+    for(i=0;i<=len;i++){
+        if(str[i]==Uppercase){
+            len.toLowercase();
+        }
+        if(str[i]==Lowercase){
+            len.toUppercase();
+        }
+    }
+}
